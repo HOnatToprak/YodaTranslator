65d6be796cd0353402ff0138a8877268dd665a0d
Hasan Onat Toprak <hasan.onat.toprak@outlook.com>
2022-07-01 00:36:12 +0300

    FunTranslationAPI free plan has call rate 5 req/h. Added in memory cache mechanism for funtranslationapi to reduce api calls.

---
+++ AFS.App/Providers/CachedFunLanguageTranslationAPIProvider.cs
@@ -0,0 +1,31 @@
+﻿using AFS.Logic;
+using Microsoft.Extensions.Caching.Memory;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("AFS.Test")]
+namespace AFS.App.Providers;
+
+class CachedFunLanguageTranslationAPIProvider : IFunLanguageTranslationProvider
+{
+    private readonly FunLanguageTranslationAPIProvider _funLanguageTranslationAPIProvider;
+    private readonly IMemoryCache _memoryCache;
+    public CachedFunLanguageTranslationAPIProvider(FunLanguageTranslationAPIProvider funLanguageTranslationAPIProvider, IMemoryCache memoryCache)
+    {
+        _funLanguageTranslationAPIProvider = funLanguageTranslationAPIProvider;
+        _memoryCache = memoryCache;
+    }
+    public async Task<string> Translate(FunLanguage language, string text)
+    {
+        // Maybe hashing the key is better?
+        string cacheKey = String.Format("Translation_{0}_{1}", Enum.GetName(language), text);
+        return await _memoryCache.GetOrCreateAsync(cacheKey, async cacheEntry =>
+        {
+            cacheEntry.SetSlidingExpiration(TimeSpan.FromHours(1));
+            return await _funLanguageTranslationAPIProvider.Translate(language, text);
+        });
+    }
+}
+
+
+
+
